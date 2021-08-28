d7230cc897b4884108bfd5d90fadfca9b940e995
Charlie441324219 <charlie19890619@gmail.com>
2021-08-28 12:29:52 -0500

    Initial commit

---
+++ src/test/java/com/example/ec2_jenkins/Ec2JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
